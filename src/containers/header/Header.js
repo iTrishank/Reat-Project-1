aad187e31bab04d1adf15a4a3bba74aaccb6e469
iTrishank <trishank9999@gmail.com>
2022-12-20 14:17:32 +0300

    project 1st componenet done

---
+++ src/containers/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="gpt3__header section__padding" id="#home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Lets built somthing amazing eith GPT-3 OpenAi
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './WhatGPT3.css';
+
+const WhatGPT3 = () => {
+  return (
+    <div>whatGPT3</div>
+  )
+}
+
+export default WhatGPT3
